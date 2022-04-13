e461a78723040b847fc23b980de816f8f4376d38
Musa Azeem <mmazeem@email.sc.edu>
2022-04-13 00:09:47 -0400

    csv backed data

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 
 from flask import Flask, render_template, request, redirect
 from markupsafe import escape
+import os
+import csv
 
 app = Flask(__name__)
 
 # Registered participants
 ppl = []
+if os.path.isfile("registrants.csv"):
+    ppl = open("registrants.csv").read().splitlines()
 
 @app.route("/")
 def homepage():
@@ -18,14 +22,14 @@ def register():
     name = request.form.get("name")
     age = request.form.get("age")
     state = request.form.get("state")
-    print(name)
-    print(age)
-    print(state)
     if not name or not age or not state:
         # check for valid input
         return render_template('invalid.html')
-    ppl.append(f"{name}, {age} from {state}")
-    return redirect("/registrants")
+    with open("registrants.csv", 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([name, age, state])
+    ppl.append(open("registrants.csv").readlines()[-1])
+    return render_template("register.html", person=ppl[-1])
 
 @app.route("/registrants")
 def registrants():
